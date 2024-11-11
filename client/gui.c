0cfc9497e9219d31495be75d2ea21486dee77f16
tung <tung@tungnx.myguest.virtualbox.org>
2024-11-11 11:13:28 +0700

    add ignore

--- client/gui.c
+++ client/gui.c
@@ -7,21 +7,33 @@ void on_button_clicked(GtkButton *button, gpointer user_data) {
 int main(int argc, char *argv[]) {
     GtkBuilder *builder;
     GtkWidget *window;
+    GtkWidget *image;
+    GdkPixbuf *pixbuf;
 
     gtk_init(&argc, &argv);
 
-    // Tạo đối tượng GtkBuilder và tải tệp Glade
+    // Load the Glade file
     builder = gtk_builder_new_from_file("client/homepage.glade");
 
-    // Lấy đối tượng window từ Glade
+    // Get the window and image widgets
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    image = GTK_WIDGET(gtk_builder_get_object(builder, "background_image"));
+
+    // Load and scale the image to fit the window size
+    pixbuf = gdk_pixbuf_new_from_file_at_scale("client/background-img.png", 400, 300, TRUE, NULL);
+    gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+
     gtk_builder_connect_signals(builder, NULL);
 
-    // Hiển thị cửa sổ
+    // Show the window
     gtk_widget_show_all(window);
 
-    // Chạy vòng lặp chính của GTK
+    // Run the main GTK loop
     gtk_main();
+
+    // Free resources
+    g_object_unref(pixbuf);
+    g_object_unref(builder);
 
     return 0;
 }
